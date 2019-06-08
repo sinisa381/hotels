9aeca704be38b4b12819758caf131a8a2f894009
Sinisa Colic <sinisa820@gmail.com>
2019-06-08 09:08:29 +0200

    netlify

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { RoomProvider } from './src/context'
 import { ThemeProvider } from 'emotion-theming'
 import theme from './src/globals/theme'
+export const onClientEntry = () => {
+	// IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+	if (typeof window.IntersectionObserver === `undefined`) {
+		import(`intersection-observer`)
+		console.log(`# IntersectionObserver is polyfilled!`)
+	}
+}
 export const wrapRootElement = ({ element }) => (
 	<ThemeProvider theme={theme}>
 		<RoomProvider>{element}</RoomProvider>
