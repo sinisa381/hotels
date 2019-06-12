c16688c305fcbc344f1e4c897b51dfaad0fe430d
Sinisa Colic <sinisa820@gmail.com>
2019-06-13 01:36:45 +0200

    font

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,105 +5,119 @@ import styled from '@emotion/styled'
 import { colors } from '../globals/colors'
 import { Flex, Box, Text, Card, Heading } from '@rebass/emotion'
 import { FaStar, FaBitcoin, FaDollarSign, FaPaypal, FaStripe, FaPhone } from 'react-icons/fa'
-
+// missing fontfamilies
 export default () => {
 	return (
 		<React.Fragment>
 			<Layout>
-				<Swiper />
-				<Flex mt={[ 4, 5, 5 ]}>
-					<Box mx='auto'>
-						<Heading fontFamily='sans' color={colors.blacks[9]} fontSize={[ 3, 4, 5 ]}>
-							Welcome to our hotel
-						</Heading>
-						<Line />
-					</Box>
-				</Flex>
-				<Flex justifyContent='space-around' flexWrap='wrap' mt={[ 4, 5, 6 ]} ml='2'>
-					<Card
-						bg={colors.green}
-						width={[ 256, 320 ]}
-						p='3'
-						borderRadius='2'
-						mb='3'
-						mr='2'
-						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
-					>
-						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
-							Accept payments with:
-						</Heading>
-						<Box>
-							<Text color={colors.whites[8]}>
-								<Payment px='4' py='3'>
-									<FaBitcoin />
-									<FaDollarSign />
-									<FaPaypal />
-									<FaStripe />
-								</Payment>
-							</Text>
+				<FontFamily>
+					<Swiper />
+					<Flex mt={[ 4, 5, 5 ]}>
+						<Box mx='auto'>
+							<Heading fontFamily='sans' color={colors.blacks[9]} fontSize={[ 3, 4, 5 ]}>
+								Welcome to our hotel
+							</Heading>
+							<Line />
 						</Box>
-					</Card>
-					<Card
-						bg={colors.green}
-						width={[ 256, 320 ]}
-						p='3'
-						borderRadius='2'
-						mb='3'
-						mr='2'
-						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
-					>
-						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
-							Free for John Wick
-						</Heading>
-						<Text color={colors.whites[8]} textAlign='center' mt='3' fontWeight='bold'>
-							Continental rules apply
-						</Text>
-						<Box />
-					</Card>
-					<Card
-						bg={colors.green}
-						width={[ 256, 320 ]}
-						p='3'
-						borderRadius='2'
-						mb='3'
-						mr='2'
-						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
-					>
-						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
-							Since 1965
-						</Heading>
-						<Text color={colors.whites[8]} fontFamily='sans' textAlign='center' mt='3' fontWeight='bold'>
-							Awarded for hospitality, luxury, food and comfort
-						</Text>
-						<Box />
-					</Card>
-					<Card
-						bg={colors.green}
-						width={[ 256, 320 ]}
-						p='3'
-						borderRadius='2'
-						mb='3'
-						mr='2'
-						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
-					>
-						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
-							<Stars color='yellow' px='2'>
-								<FaStar />
-								<FaStar />
-								<FaStar />
-								<FaStar />
-								<FaStar />
-							</Stars>
-							<Text mb='2'>We offer best services</Text>
-							<Text color={colors.whites[8]}>
-								<Flex justifyContent='center' alignItems='center'>
-									<FaPhone />
-									<Text ml='10px'>000-000-000</Text>
-								</Flex>
+					</Flex>
+					<Flex justifyContent='space-around' flexWrap='wrap' mt={[ 4, 5, 6 ]} ml='2'>
+						<Card
+							bg={colors.green}
+							width={[ 256, 320 ]}
+							p='3'
+							borderRadius='2'
+							mb='3'
+							mr='2'
+							boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+						>
+							<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
+								Accept payments with:
+							</Heading>
+							<Box>
+								<Text color={colors.whites[8]}>
+									<Payment px='4' py='3'>
+										<FaBitcoin />
+										<FaDollarSign />
+										<FaPaypal />
+										<FaStripe />
+									</Payment>
+								</Text>
+							</Box>
+						</Card>
+						<Card
+							bg={colors.green}
+							width={[ 256, 320 ]}
+							p='3'
+							borderRadius='2'
+							mb='3'
+							mr='2'
+							boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+						>
+							<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
+								Free for John Wick
+							</Heading>
+							<Text
+								color={colors.whites[8]}
+								fontFamily='sans'
+								textAlign='center'
+								mt='3'
+								fontWeight='bold'
+							>
+								Continental rules apply if you have the coin
 							</Text>
-						</Heading>
-					</Card>
-				</Flex>
+							<Box />
+						</Card>
+						<Card
+							bg={colors.green}
+							width={[ 256, 320 ]}
+							p='3'
+							borderRadius='2'
+							mb='3'
+							mr='2'
+							boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+						>
+							<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
+								Since 1965
+							</Heading>
+							<Text
+								color={colors.whites[8]}
+								fontFamily='sans'
+								textAlign='center'
+								mt='3'
+								fontWeight='bold'
+							>
+								Awarded for hospitality, luxury, food and comfort
+							</Text>
+							<Box />
+						</Card>
+						<Card
+							bg={colors.green}
+							width={[ 256, 320 ]}
+							p='3'
+							borderRadius='2'
+							mb='3'
+							mr='2'
+							boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+						>
+							<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
+								<Stars color='yellow' px='2'>
+									<FaStar />
+									<FaStar />
+									<FaStar />
+									<FaStar />
+									<FaStar />
+								</Stars>
+								<Text mb='2'>We offer best services</Text>
+								<Text color={colors.whites[8]}>
+									<Flex justifyContent='center' alignItems='center'>
+										<FaPhone />
+										<Text ml='10px'>000-000-000</Text>
+									</Flex>
+								</Text>
+							</Heading>
+						</Card>
+					</Flex>
+				</FontFamily>
 			</Layout>
 		</React.Fragment>
 	)
@@ -127,3 +141,4 @@ const Line = styled.div`
 	margin: 0 auto;
 	margin-top: 1rem;
 `
+const FontFamily = styled.div`font-family: system-ui, sans-serif;`
