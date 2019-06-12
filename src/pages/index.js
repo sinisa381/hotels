118af9d516eb7b88977f197ff9773eb83a6e46f6
Sinisa Colic <sinisa820@gmail.com>
2019-06-13 01:25:35 +0200

    colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,19 +13,27 @@ export default () => {
 				<Swiper />
 				<Flex mt={[ 4, 5, 5 ]}>
 					<Box mx='auto'>
-						<Heading fontFamily='sans' color='blacks.9' fontSize={[ 3, 4, 5 ]}>
+						<Heading fontFamily='sans' color={colors.blacks[9]} fontSize={[ 3, 4, 5 ]}>
 							Welcome to our hotel
 						</Heading>
 						<Line />
 					</Box>
 				</Flex>
 				<Flex justifyContent='space-around' flexWrap='wrap' mt={[ 4, 5, 6 ]} ml='2'>
-					<Card bg={colors.green} width={[ 256, 320 ]} p='3' borderRadius='2' mb='3' mr='2' boxShadow='five'>
-						<Heading fontFamily='sans' textAlign='center' color='blacks.8'>
+					<Card
+						bg={colors.green}
+						width={[ 256, 320 ]}
+						p='3'
+						borderRadius='2'
+						mb='3'
+						mr='2'
+						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+					>
+						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
 							Accept payments with:
 						</Heading>
 						<Box>
-							<Text color='whites.8'>
+							<Text color={colors.whites[8]}>
 								<Payment px='4' py='3'>
 									<FaBitcoin />
 									<FaDollarSign />
@@ -35,26 +43,50 @@ export default () => {
 							</Text>
 						</Box>
 					</Card>
-					<Card bg={colors.green} width={[ 256, 320 ]} p='3' borderRadius='2' mb='3' mr='2' boxShadow='five'>
-						<Heading fontFamily='sans' textAlign='center' color='blacks.8'>
+					<Card
+						bg={colors.green}
+						width={[ 256, 320 ]}
+						p='3'
+						borderRadius='2'
+						mb='3'
+						mr='2'
+						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+					>
+						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
 							Free for John Wick
 						</Heading>
-						<Text color='whites.8' textAlign='center' mt='3' fontWeight='bold'>
+						<Text color={colors.whites[8]} textAlign='center' mt='3' fontWeight='bold'>
 							Continental rules apply
 						</Text>
 						<Box />
 					</Card>
-					<Card bg={colors.green} width={[ 256, 320 ]} p='3' borderRadius='2' mb='3' mr='2' boxShadow='five'>
-						<Heading fontFamily='sans' textAlign='center' color='blacks.8'>
+					<Card
+						bg={colors.green}
+						width={[ 256, 320 ]}
+						p='3'
+						borderRadius='2'
+						mb='3'
+						mr='2'
+						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+					>
+						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
 							Since 1965
 						</Heading>
-						<Text color='whites.8' fontFamily='sans' textAlign='center' mt='3' fontWeight='bold'>
+						<Text color={colors.whites[8]} fontFamily='sans' textAlign='center' mt='3' fontWeight='bold'>
 							Awarded for hospitality, luxury, food and comfort
 						</Text>
 						<Box />
 					</Card>
-					<Card bg={colors.green} width={[ 256, 320 ]} p='3' borderRadius='2' mb='3' mr='2' boxShadow='five'>
-						<Heading fontFamily='sans' textAlign='center' color='blacks.8'>
+					<Card
+						bg={colors.green}
+						width={[ 256, 320 ]}
+						p='3'
+						borderRadius='2'
+						mb='3'
+						mr='2'
+						boxShadow='4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )'
+					>
+						<Heading fontFamily='sans' textAlign='center' color={colors.blacks[8]}>
 							<Stars color='yellow' px='2'>
 								<FaStar />
 								<FaStar />
@@ -63,7 +95,7 @@ export default () => {
 								<FaStar />
 							</Stars>
 							<Text mb='2'>We offer best services</Text>
-							<Text color='whites.8'>
+							<Text color={colors.whites[8]}>
 								<Flex justifyContent='center' alignItems='center'>
 									<FaPhone />
 									<Text ml='10px'>000-000-000</Text>
